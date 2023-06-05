5833b8a66c6566bd3fc1249360b354d2e11a8a34
Ana Carolina <carol.oliva.cruz@gmail.com>
2023-06-05 18:41:22 -0300

    feat: home base

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import { Inter } from "next/font/google";
 import Footer from "@/components/footer";
 
-
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
@@ -9,7 +8,42 @@ export default function Home() {
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-      <Footer/>
+      {/* <img src="" alt="" /> 
+      componente de filtros = {
+        <ul>
+           <li>
+              <h4>Nome categoria</h4>
+              <ul>
+                  <li>
+                    <button>filtro</button>
+                  </li>
+              </ul>
+           </li>
+        </ul>
+        <></>
+      }
+      componente listCards = {
+        <ul>
+          <li>
+              <img src="" alt="" />
+              <h7>Nome do carro</h7>
+              <p>Descrição do carro</p>
+              <div>
+                 <img src="" alt="" />
+                 <p>Vendedor</p>
+              </div>
+              <div>
+                 <span>km</span>
+                 <span>Ano</span>
+                 <h7>Valor</h7>
+              </div>
+          </li>
+        </ul>
+        --pagination--
+      }
+      */}
+
+      <Footer />
     </main>
   );
 }
