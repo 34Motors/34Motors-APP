64d49dee3eb2592a20c2a5bbf052920e5c3e9539
Ana Carolina <carol.oliva.cruz@gmail.com>
2023-06-06 14:54:40 -0300

    feat: home base

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,51 @@ export default function Home() {
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    ></main>
+    >
+      {/* <img src="" alt="" /> 
+
+        --se mobile: 
+        componente listCards
+        button que abre modal filter
+        pagination
+        --
+
+      componente de filtros = {
+        <ul>
+           <li>
+              <h4>Nome categoria</h4>
+              <ul>
+                  <li>
+                    <button>filtro</button>
+                  </li>
+              </ul>
+           </li>
+        </ul>
+        <></>
+      }
+
+      componente listCards = {
+        <aside>
+         <ul>
+          <li>
+              <img src="" alt="" />
+              <h7>Nome do carro</h7>
+              <p>Descrição do carro</p>
+              <div>
+                 <img src="" alt="" />
+                 <p>Vendedor</p>
+              </div>
+              <div>
+                 <span>km</span>
+                 <span>Ano</span>
+                 <h7>Valor</h7>
+              </div>
+          </li>
+        </ul>
+        </aside>
+        --pagination--
+      }
+      */}
+    </main>
   );
 }
