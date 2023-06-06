05b50902bc0dea5d5e7aba85939c84e7766aed7b
Olivio Freitas <ojfneto92@gmail.com>
2023-06-06 13:56:13 -0300

    feat: create modal base

---
+++ src/components/modalBase.tsx
@@ -0,0 +1,34 @@
+import React, { ReactNode, useRef, useEffect } from "react";
+import { createPortal } from "react-dom";
+
+interface ModalProps {
+  toggleModal: () => void;
+  blockClosing?: boolean;
+  children: ReactNode;
+}
+
+const ModalBase = ({ toggleModal, blockClosing, children }: ModalProps) => {
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClick = (event: MouseEvent) => {
+      if (!ref.current) return;
+
+      if (!event.target) return;
+
+      if (!ref.current.contains(event.target as HTMLDivElement)) toggleModal();
+    };
+
+    window.addEventListener("mousedown", handleClick);
+
+    return () => {
+      window.removeEventListener("mousedown", handleClick);
+    };
+  }, [toggleModal]);
+  return createPortal(
+    <div ref={blockClosing ? null : ref}>{children}</div>,
+    document.body
+  );
+};
+
+export default ModalBase;
