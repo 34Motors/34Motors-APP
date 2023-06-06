a1e5f868f991e4c8e1528790982bd6afb3e00429
Olivio Freitas <ojfneto92@gmail.com>
2023-06-06 11:28:22 -0300

    feat: added header buttons

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,8 +2,9 @@ import { useState, useEffect } from "react";
 import Image from "next/image";
 import logo from "../assets/logo-34-motors-black.png";
 import { IoMdMenu } from "react-icons/io";
+import Link from "next/link";
 
-const Header = () => {
+const  Header = () => {
   const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
@@ -20,13 +21,17 @@ const Header = () => {
 
   return (
     <div
-      className={`w-full flex justify-between items-center bg-white p-[45px]`}
+      className={`w-full flex justify-between items-center bg-white h-[80px] sticky top-0 `}
     >
       <Image width={200} src={logo} alt="Logo 34 Motors" />
       {isMobile ? (
           <IoMdMenu className={`text-black text-3xl`} />
       ) : (
-        <button className="bg-black">Componente de usuário</button>
+        <div className="flex items-center gap-11">
+          <div className={`border-l-2 border-gray-300 h-[80px]`}></div>
+          <Link href={"/"} className={`text-gray-600`}>Fazer Login</Link>
+          <Link href={"/register"} className={`text-gray-800 border-2 py-3 px-7 rounded`}>Cadastrar</Link>
+        </div>
       )}
     </div>
   );
