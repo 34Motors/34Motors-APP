5677322866e1c35e1392bbffe021872dd58fa244
Olivio Freitas <ojfneto92@gmail.com>
2023-06-14 13:54:24 -0300

    feat: scroll to top button working

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,6 +3,12 @@ import logo from "../assets/logo-34-motors-white.png";
 import { MdKeyboardArrowUp } from "react-icons/md";
 
 const Footer = () => {
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    })
+  }
   return (
     <div
       className={`w-full flex flex-col gap-14 md:justify-between md:flex-row items-center bg-grey-0 p-[45px]`}
@@ -11,7 +17,7 @@ const Footer = () => {
       <h1 className={`text-grey-whiteFixed text-body2 font-400 text-center`}>
         34 Motors© 2023 - Todos os direitos reservados.
       </h1>
-      <button className={`px-5 py-4 rounded bg-grey-1 flex items-center justify-center`}>
+      <button onClick={scrollToTop} className={`px-5 py-4 rounded bg-grey-1 flex items-center justify-center`}>
         <MdKeyboardArrowUp
           className={`text-heading7 text-grey-whiteFixed`}
         />
