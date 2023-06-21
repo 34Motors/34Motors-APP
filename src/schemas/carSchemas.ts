d6a927ba70e50b0bee9eba1eb2330cbbb8af900d
Isadora Perdigão <102971900+IsadoraPerdigao@users.noreply.github.com>
2023-06-21 14:47:08 -0300

    Return car schema to past version

--- src/schemas/carSchemas.ts
+++ src/schemas/carSchemas.ts
@@ -1,34 +1,19 @@
 import { z } from "zod";
 
 const createCarBody = z.object({
-  cars: z.object({
-    brand: z.string().max(20),
-    model: z.string().max(50),
-    year: z.string().max(4),
-    fuelType: z.enum(["Hibrido", "Flex", "Eletrico"]),
-    quilometers: z.number(),
-    color: z.string().max(16),
-    fipePrice: z.string(),
-    price: z.number(),
-    description: z.string(),
-    frontImage: z.string().max(255),
-    user: z
-      .object({
-        name: z.string(),
-      })
-      .array(),
-    published: z.boolean().default(true),
-    userId: z.number(),
-  }),
-  filteredOptions: z.object({
-    brands: z.string().array(),
-    colors: z.string().array(),
-    fuelTypes: z.string().array(),
-    maxPrice: z.number(),
-    minPrice: z.number(),
-    minQuilometers: z.number(),
-    years: z.string().array()
-  })
+  brand: z.string().max(20),
+  model: z.string().max(50),
+  year: z.string().max(4),
+  fuelType: z.enum(["Hibrido", "Flex", "Eletrico"]),
+  quilometers: z.string().max(6),
+  color: z.string().max(16),
+  fipePrice: z.string(),
+  price: z.number(),
+  description: z.string(),
+  frontImage: z.string().max(255),
+  images: z.string().array(),
+  published: z.boolean().default(true),
+  userId: z.number(),
 });
 
 const returnCar = createCarBody.extend({
