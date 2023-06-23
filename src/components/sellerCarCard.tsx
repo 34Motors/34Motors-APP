6765e3eda7250102237c6b729bed7df89f4d7297
Paulo Moreno <106778522+PauloMorenoD@users.noreply.github.com>
2023-06-23 15:53:13 -0300

    fix : change car description height

--- src/components/sellerCarCard.tsx
+++ src/components/sellerCarCard.tsx
@@ -29,7 +29,7 @@ const SellerCarCard = ({
   price,
 }: iCarCarProps) => {
   return (
-    <li className="w-[312px] md:w-[40%] md:max-w-[312px] relative">
+    <li className="min-w-[320px] h-[280px] md:w-[40%] md:max-w-[312px] relative">
       <span
         className={
           published
@@ -52,7 +52,7 @@ const SellerCarCard = ({
         <h6 className="font-lexend font-600 text-heading7 text-grey-1 capitalize mt-4">
           {brand} - {model}
         </h6>
-        <p className="text-sm overflow-hidden text-ellipsis text-grey-2 leading-6 font-inter font-400">
+        <p className="text-sm h-12 overflow-hidden text-ellipsis text-grey-2 leading-6 font-inter font-400">
           {description}
         </p>
         <div className=" flex justify-between items-center">
